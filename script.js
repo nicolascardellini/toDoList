a8ef2ae88b3501aa457d63d3fb7d40c5ca9e35f9
nicolascardellini <74785599+nicolascardellini@users.noreply.github.com>
2021-03-22 19:37:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.getElementById("input");
 const table = document.getElementById("tabla");
 const boton = document.getElementById("boton");
 const form = document.getElementById("form");
+const screenbtn = document.getElementById("startFull");
 let tareas = [];
 function agregarTarea() {
   const fila = document.createElement("tr");
@@ -155,3 +156,85 @@ function showPosition(position) {
   console.log((position.coords.latitude + ", " + position.coords.longitude).innerHTML);
 }
 
+var $ = document.querySelector.bind(document);
+var $$ = function (selector) {
+  return [].slice.call(document.querySelectorAll(selector), 0);
+}
+
+
+
+
+var prefix = null;
+if ('requestFullscreen' in document.documentElement) {
+  prefix = 'fullscreen';
+} else if ('mozRequestFullScreen' in document.documentElement) {
+  prefix = 'mozFullScreen';
+} else if ('webkitRequestFullscreen' in document.documentElement) {
+  prefix = 'webkitFullscreen';
+} else if ('msRequestFullscreen') {
+  prefix = 'msFullscreen';
+}
+
+var onFullscreenChange = function () {
+  var elementName = 'not set';
+  if (document[prefix + 'Element']) {
+    elementName = document[prefix + 'Element'].nodeName;
+  }
+  if(elementName ==='HTML'){
+    screenbtn.className ="fas fa-compress";
+  }else{
+    screenbtn.className = "fas fa-expand";
+  }
+  
+  
+  logChange('New fullscreen element is ' + elementName + '');
+  onFullscreenHandler(!!document[prefix + 'Element']);
+}
+
+if (document[prefix + 'Enabled']) {
+  var onFullscreenHandler = function (started) {
+    $('#exit').style.display = started ? 'inline-block' : 'none';
+    $$('.start').forEach(function (x) {
+      x.style.display = started ? 'none' : 'inline-block';
+    });
+  };
+
+  document.addEventListener(prefix.toLowerCase() + 'change', onFullscreenChange);
+
+  var goFullScreen = null;
+  var exitFullScreen = null;
+  if ('requestFullscreen' in document.documentElement) {
+    goFullScreen = 'requestFullscreen';
+    exitFullScreen = 'exitFullscreen';
+  } else if ('mozRequestFullScreen' in document.documentElement) {
+    goFullScreen = 'mozRequestFullScreen';
+    exitFullScreen = 'mozCancelFullScreen';
+  } else if ('webkitRequestFullscreen' in document.documentElement) {
+    goFullScreen = 'webkitRequestFullscreen';
+    exitFullScreen = 'webkitExitFullscreen';
+  } else if ('msRequestFullscreen') {
+    goFullScreen = 'msRequestFullscreen';
+    exitFullScreen = 'msExitFullscreen';
+  }
+
+  var goFullscreenHandler = function (element) {
+    return function () {
+      var maybePromise = element[goFullScreen]();
+      if (maybePromise && maybePromise.catch) {
+        maybePromise.catch(function (err) {
+          logChange('Cannot acquire fullscreen mode: ' + err);
+        });
+      }
+    };
+  };
+
+
+      $('#startFull').addEventListener('click', goFullscreenHandler(document.documentElement));
+      $('#startFull').addEventListener('click', function () {
+        document[exitFullScreen]();
+      });
+ 
+  
+
+  
+};
